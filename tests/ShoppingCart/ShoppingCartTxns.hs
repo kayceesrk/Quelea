3f07cca8216603d7a20ef91a222f9c2d70def633
gowthamk <gowtham@purdue.edu>
2014-10-15 12:55:27 -0400

    reviewCart done

--- tests/ShoppingCart/ShoppingCartTxns.hs
+++ tests/ShoppingCart/ShoppingCartTxns.hs
@@ -46,22 +46,25 @@ removeFromCart cartID itemID qty =
     ilist::[(ItemID,Int)] <- invoke key GetItemsInCart ()
     return ilist
 
-
 inStock :: (ItemID,Int) -> CSN Bool
 inStock (itemId,qty) = do
   (avlblQty::Int) <- invoke (mkKey itemId) ShowItem ()
   return (avlblQty >= qty)
 
-{-reviewCart :: CartID -> CSN [(ItemID,Int)]
+checkStock :: [(ItemID,Int)] -> CSN [(ItemID,Int)]
+checkStock ilist = foldM f [] ilist 
+  where
+    f acc x = do
+      yes <- inStock x
+      return $ (if yes then x else error "Fail"):acc
+  
+reviewCart :: CartID -> CSN [(ItemID,Int)]
 reviewCart cartID = 
   atomically ($(checkTxn "_reviewCartTxn" reviewCartTxnCtrt)) $ do
     let key = mkKey cartID
     ilist::[(ItemID,Int)] <- invoke key GetItemsInCart ()
-    return $ map (\x -> if inStock x then x else error "Fail") ilist
--}
+    checkStock ilist
 
-reviewCart :: ()
-reviewCart = ()
 
 payAndCheckout :: ()
 payAndCheckout = ()
