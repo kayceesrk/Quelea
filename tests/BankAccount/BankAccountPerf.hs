90f9fb945fd655cca59acce9d801d50239c1cc65
KC Sivaramakrishnan <chandras@cs.purdue.edu>
2014-10-24 12:59:03 +0100

    Added BA perf

---
+++ tests/BankAccount/BankAccountPerf.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+
+import Codeec.Shim
+import Codeec.ClientMonad
+import Codeec.DBDriver
+import BankAccountDefs
+import Codeec.Contract
+import System.Process (runCommand, terminateProcess)
+import System.Environment (getExecutablePath, getArgs)
+import Control.Concurrent (threadDelay)
+import Codeec.NameService.SimpleBroker
+import Codeec.Marshall
+import Codeec.TH
+import Database.Cassandra.CQL
+import Control.Monad.Trans (liftIO)
+import Data.Text (pack)
+import Codeec.Types (summarize)
+import Control.Monad (replicateM_, forever, when)
+import Data.IORef
+
+fePort :: Int
+fePort = 5558
+
+bePort :: Int
+bePort = 5559
+
+
+data Kind = B | C | S | D deriving (Read, Show)
+
+keyspace :: Keyspace
+keyspace = Keyspace $ pack "Codeec"
+
+dtLib = mkDtLib [(Deposit, mkGenOp deposit summarize, $(checkOp Deposit depositCtrt)),
+                 (Withdraw, mkGenOp withdraw summarize, $(checkOp Withdraw withdrawCtrt)),
+                 (GetBalance, mkGenOp getBalance summarize, $(checkOp GetBalance getBalanceCtrt))]
+
+main :: IO ()
+main = do
+  (kindStr:_) <- getArgs
+  let k :: Kind = read kindStr
+  case k of
+    B -> startBroker (Frontend $ "tcp://*:" ++ show fePort)
+                     (Backend $ "tcp://*:" ++ show bePort)
+    S -> do
+      runShimNode dtLib [("localhost","9042")] keyspace
+        (Backend $ "tcp://localhost:" ++ show bePort) 5560
+    C -> runSession (Frontend $ "tcp://localhost:" ++ show fePort) $ do
+      key <- liftIO $ newKey
+      iter <- liftIO $ newIORef (0::Int)
+      forever $ do
+        r::() <- invoke key Deposit (1::Int)
+        r::Int <- invoke key GetBalance ()
+        c <- liftIO $ atomicModifyIORef iter (\c -> (c+1,c+1))
+        when (c `mod` 100 == 0) (liftIO . putStrLn $ "iter=" ++ show c)
+    D -> do
+      pool <- newPool [("localhost","9042")] keyspace Nothing
+      runCas pool $ createTable "BankAccount"
+      progName <- getExecutablePath
+      putStrLn "Driver : Starting broker"
+      b <- runCommand $ progName ++ " B"
+      putStrLn "Driver : Starting server"
+      s <- runCommand $ progName ++ " S"
+      putStrLn "Driver : Starting client"
+      c <- runCommand $ progName ++ " C"
+      threadDelay 5000000
+      mapM_ terminateProcess [b,s,c]
+      runCas pool $ dropTable "BankAccount"
