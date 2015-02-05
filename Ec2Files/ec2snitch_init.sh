dd9127079fdfe4afdbcb3374ff816acbfdb7c796
KC Sivaramakrishnan <chandras@cs.purdue.edu>
2015-02-05 17:55:15 +0000

    Minor edit to script

--- Ec2Files/ec2snitch_init.sh
+++ Ec2Files/ec2snitch_init.sh
@@ -5,7 +5,7 @@ SEEDS=$1
 CLUSTERNAME=QueleaCluster
 printf "127.0.0.1 localhost\n127.0.0.1 `hostname`\n" > /etc/hosts
 MYIP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-mv cassandra.yaml.ec2snitch_dsc_2_0_12 cassandra.yaml
+cp cassandra.yaml.ec2snitch_dsc_2_0_12 cassandra.yaml
 perl -p -i -e "s/__NAME/$CLUSTERNAME/g" cassandra.yaml
 perl -p -i -e "s/__MYIP/$MYIP/g" cassandra.yaml
 perl -p -i -e "s/__SEEDS/$SEEDS/g" cassandra.yaml
