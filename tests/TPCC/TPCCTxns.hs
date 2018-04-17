55ce93cdba2ebfaf035456be5a57da6a0fed7cb3
KC Sivaramakrishnan <kc@kcsrk.info>
2018-04-17 05:25:03 +0000

    Obtained keys

--- tests/TPCC/TPCCTxns.hs
+++ tests/TPCC/TPCCTxns.hs
@@ -44,7 +44,7 @@ import System.IO (hFlush, stdout)
       return le)
 
 doNewOrderTxn :: DistrictID -> WarehouseID -> Int -> CSN (Int)
-doNewOrderTxn did wid ireq = 
+doNewOrderTxn did wid ireq =
   let ireqs = [1..ireq] in
   atomically (doNewOrderTxnCtrtA) $ do
     nextoid::Int <- invoke (mkKey (did,wid)) GetAndIncNextOID ()
@@ -53,7 +53,7 @@ doNewOrderTxn did wid ireq =
     return nextoid
 
 doPaymentTxn :: Int -> WarehouseID -> DistrictID -> CustomerID -> CSN ()
-doPaymentTxn h_amt wid cdid cid = 
+doPaymentTxn h_amt wid cdid cid =
   atomically (doPaymentTxnCtrtA) $ do
     r::() <- invoke (mkKey wid) AddYtd (h_amt)
     r::() <- invoke (mkKey (cid,cdid,wid)) AddCustomerBal (-1*h_amt)
@@ -72,12 +72,10 @@ doDeliveryTxn wid did oid = do
 {-Invariant corresponds to doPaymentTxn-}
 {-W_YTD = sum(H_AMOUNT) where history entry's cwid = this warehouse's id-}
 inv1 :: WarehouseID -> CSN (Bool)
-inv1 wid = 
-  {-keys ks = All keys of history table-}
-  let ks::[(CustomerID, DistrictID, WarehouseID)] = [] in
-  do
+inv1 wid = do
+  ks::[(CustomerID, DistrictID, WarehouseID)] <- getKeys "HistoryEffect"
   w_ytd::Int <- invoke (mkKey (wid)) GetYtd ()
-  h_amt::Int <- foldM (\ s (cid,cdid,cwid) -> 
+  h_amt::Int <- foldM (\ s (cid,cdid,cwid) ->
                         if cwid==wid then
                           do
                           r::Int <- invoke (mkKey (cid,cdid,cwid)) GetHistoryAmt ()
@@ -86,20 +84,17 @@ inv1 wid =
   return $ w_ytd==h_amt
 
 {- Invariant corresponds to doNewOrderTxn -}
-{- District's NEXT_O_ID - 1 = max(OrderID) where OrderID's district is this 
+{- District's NEXT_O_ID - 1 = max(OrderID) where OrderID's district is this
    District'd did and OrderID's warehouse is this district's warehouse(dwid) . -}
 inv2 :: WarehouseID -> DistrictID -> CSN (Bool)
-inv2 did dwid = 
-  {-keys ks_d = All keys of district table-}
-  let ks_d = [] in
-  {-keys ks_o = All keys of orders table-}
-  let ks_o = [] in
-  do
-  max_oid::Int <- foldM (\ s (o_id,o_d_id,o_w_id) -> 
+inv2 (WarehouseID did) (DistrictID dwid) = do
+  ks_d::[(DistrictID, WarehouseID)] <- getKeys "DistrictEffect"
+  ks_o::[(OrderID, DistrictID, WarehouseID)] <- getKeys "OrderEffect"
+  max_oid::Int <- foldM (\ s (OrderID o_id, DistrictID o_d_id, WarehouseID o_w_id) ->
                         if o_d_id==did && o_w_id==dwid then
                           if o_id > s then return o_id else return s
                         else return s) (-1) ks_o
-  d_nextoid::Int <- foldM (\ s (d_id,d_w_id) -> 
+  d_nextoid::Int <- foldM (\ s (DistrictID d_id, WarehouseID d_w_id) ->
                         if did==d_id && dwid==d_w_id then
                           do
                           nextoid::Int <- invoke (mkKey (d_id,d_w_id)) GetAndIncNextOID (False)
@@ -109,17 +104,15 @@ inv2 did dwid =
   return $ max_oid == d_nextoid-1
 
 {- Invariant corresponds to doDeliveryTxn -}
-{-For any row in the ORDER-LINE table, DeliveryDate is set 
-   if and only if the corresponding row in the ORDER table defined by 
-   (O_W_ID, O_D_ID, O_ID) = (OL_W_ID, OL_D_ID, OL_O_ID) 
+{-For any row in the ORDER-LINE table, DeliveryDate is set
+   if and only if the corresponding row in the ORDER table defined by
+   (O_W_ID, O_D_ID, O_ID) = (OL_W_ID, OL_D_ID, OL_O_ID)
    has O_CARRIER_ID set-}
 inv3 :: WarehouseID -> DistrictID -> OrderID -> CSN (Bool)
-inv3 o_w_id o_d_id o_id = 
-  {-keys ks_ol = All keys of Orderline table-}
-  let ks_ol::[(OrderID, DistrictID, WarehouseID, Int)] = [] in
-  do
+inv3 o_w_id o_d_id o_id = do
+  ks_ol::[(OrderID, DistrictID, WarehouseID, Int)] <- getKeys "OrderlineEffect"
   o_carrier_set::Bool <- invoke (mkKey (o_id,o_d_id,o_w_id)) CheckCarrierSet ()
-  ol_delivery_set::Bool <- foldM (\ s ((ol_o_id,ol_d_id,ol_w_id,olnum)) -> 
+  ol_delivery_set::Bool <- foldM (\ s ((ol_o_id,ol_d_id,ol_w_id,olnum)) ->
                 if o_d_id==ol_d_id && o_w_id==ol_w_id && o_id==ol_o_id then
                   do
                   r::Bool <- invoke (mkKey (ol_o_id,ol_d_id,ol_w_id,olnum)) CheckDeliverySet ()
@@ -133,6 +126,6 @@ inv3 o_w_id o_d_id o_id =
   First, customer buys some stuff
   oid <- doNewOrderTxn did wid ireq //ireq is a random number > 0
   Then, the stuff gets delivered
-  () <- doDeliveryTxn wid did oid 
+  () <- doDeliveryTxn wid did oid
   Then, the customer pays
   () <- doPaymentTxn h_amt wid did cid //h_amt is a random number-}
